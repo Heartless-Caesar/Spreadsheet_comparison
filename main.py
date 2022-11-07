311523574676671ede4d0e1b03d146a195c53ef6
cesa <joaovictorcsatu@gmail.com>
2022-11-07 16:55:49 -0300

    update

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton,QLabel, QLineEdit, QVBoxLayout,QWidget,QInputDialog,QFileDialog,QDialog
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QPushButton,QLabel, QFileDialog,QDialog,QComboBox,QMainWindow, QMessageBox
+from display_sheet import display_sheet
 from PyQt5 import uic
-from compare_sheets import ProcessarDados
-import sys
-import os
-
-# otherDirectory = os.chdir("")
-
-# currentDirectory = os.getcwd()
 
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow,self).__init__()
 
         # Carrega o arquivo UI que será utilizado
-        uic.loadUi("sheets.ui",self)
+        self.window = uic.loadUi("sheets.ui",self)
         
+        # CArrega tela para alertas diversos
+        self.message = QMessageBox()
+
         # Propriedades que irão ter o nome do arquivo para o pandas processar
         self.fname_one = ""
         self.fname_two = ""
+
+        # Selecionar se os arquivos tem o mesmo numero de 
+        self.combobox = self.findChild(QComboBox,"comboBox")
+        self.combobox.addItems(["Sim","Não"])
 
         # Elementos do UI que serão manipulados de alguma forma
         self.select_file_one_button = self.findChild(QPushButton,"select_file_one_button")
@@ -39,21 +38,31 @@ class MainWindow(QDialog):
 
         # Executa comparacao de planilhas
         self.execute_button.clicked.connect(self.executar_processamento)
+        self.execute_button.clicked.connect(self.open_second_window)
+        self.execute_button.clicked.connect(lambda _, xl_path="test",sheet_name="tes":display_sheet.load_excel_data(xl_path,sheet_name))
 
     def open_file_dialog_one(self):
-         self.fname_one = QFileDialog.getOpenFileName(self, "Selecione um arquivo","","(*.xlsx);;All Files (*)")   
+         self.fname_one = QFileDialog.getOpenFileName(self, "Selecione um arquivo","","(*.xlsx);;(*.ods);;All Files (*)")   
 
          if self.fname_one:
             self.label_one.setText(str(self.fname_one).split("/")[-1].split("'")[0])
 
     def open_file_dialog_two(self):
-         self.fname_two = QFileDialog.getOpenFileName(self, "Selecione um arquivo","","(*.xlsx);;All Files (*)")   
+         self.fname_two = QFileDialog.getOpenFileName(self, "Selecione um arquivo","","(*.xlsx);;(*.ods);;All Files (*)")   
 
          if self.fname_two:
             self.label_two.setText(str(self.fname_two).split("/")[-1].split("'")[0])  
 
     def executar_processamento(self):
-        ProcessarDados.compare_sheets(self.fname_one,self.fname_two)              
+        if (self.fname_one or self.fname_two != ""): 
+            display_sheet.compare_sheets(str(self.fname_one).split("/")[-1].split("'")[0],str(self.fname_two).split("/")[-1].split("'")[0])   
+        self.message.setText("One of the directories is empty")
+        #self.message.show()
+        return        
+
+    def open_second_window(self):
+       self.ui = display_sheet(self)
+       self.ui.show()
 
 
 # Inicializa a aplicação
@@ -61,6 +70,8 @@ def start_app():
     # Aplicação em si
     app = QApplication([])
 
+    app.setStyle('Fusion')
+    
     # Janela Principal
     window = MainWindow()
 
